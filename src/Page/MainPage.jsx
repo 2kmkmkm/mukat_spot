1d28e92a3f6ff9fb48224b7d9611b22863e9301e
2kmkm <02dlrudals@gmail.com>
2024-03-19 13:47:22 +0900

    feat: 메인페이지 대학교 표시 구현

--- src/Page/MainPage.jsx
+++ src/Page/MainPage.jsx
@@ -3,10 +3,14 @@ import Category from "../components/Category";
 import logo_mukat from "../Img/logo_mukat.svg";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import "../components/main.css";
+import point from "../Img/point.svg";
+import icon_search from "../Img/icon_serach.svg";
+import mukat from "../Img/mukat.svg";
+import logo_mukatlist from "../Img/logo_mukatlist.svg";
 
 const Logo = styled.a`
-  padding-left: 20px;
-  width: 100px;
+  padding: 15px;
 `;
 
 const Header = styled.div`
@@ -16,6 +20,44 @@ const Header = styled.div`
   background-color: none;
   box-shadow: 0 0 5px 0 gray;
   align-items: center;
+`;
+
+const Point = styled.a`
+  padding: 15px;
+  padding-top: 18px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Univ = styled.div`
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+  width: 340px;
+  height: 180px;
+  border: solid #fff3ef;
+  border-radius: 2rem;
+`;
+
+const UnivName = styled.div`
+  display: flex;
+  margin-top: 15px;
+  height: 20px;
+  background-color: #fe6a38;
+  width: 120px;
+  border: none;
+  border-radius: 2rem;
+  color: white;
+  text-align: center;
+  font-size: 10px;
+  align-items: center;
+  justify-content: center;
 `;
 
 const MainPage = () => {
@@ -49,19 +91,31 @@ const MainPage = () => {
           value={search}
           onChange={onChange}
           placeholder="음식점을 입력해주세요"
-        />
+        ></input>
       </div>
     );
   };
 
   return (
     <>
-      <Header>
-        <Logo to="..">
-          <img src={logo_mukat} alt="logo" />
-        </Logo>
-        <SearchBar />
-      </Header>
+      <div>
+        <Header>
+          <Logo to="..">
+            <img src={logo_mukat} alt="logo" />
+          </Logo>
+          <SearchBar />
+          <Point>
+            <img src={point} alt="point" />
+          </Point>
+        </Header>
+        <Wrapper>
+          <Univ>
+            <img width="50px" src={mukat} alt="mukat" />
+            <img width="100px" src={logo_mukatlist} alt="logo_mukatlist" />
+            <UnivName>구름대학교</UnivName>
+          </Univ>
+        </Wrapper>
+      </div>
     </>
   );
 };
